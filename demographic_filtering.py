221b4cd695b68187357ec34e1eafa094a4dfeffa
awesomeayaan13 <41842116+awesomeayaan13@users.noreply.github.com>
2022-10-13 19:47:02 -0400

    Add files via upload

---
+++ demographic_filtering.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+
+df=pd.read_csv('articles.csv',encoding="utf-8")
+
+df=df.sort_values(['total_events'],ascending=False)
+output=df[['url','title','text','lang','total_events']].head(20).values.tolist()
